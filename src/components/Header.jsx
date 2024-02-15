00bf9794afcde8a8487b4ca738f7bc99299f98c3
kanishka36 <kanishkaj36@gmail.com>
2024-02-15 14:14:27 +0530

    install flobite

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import { Navbar } from "flowbite-react";
+import React from "react";
+
+const Header = () => {
+  return <Navbar className="border-b-2">Navbar</Navbar>;
+};
+
+export default Header;
